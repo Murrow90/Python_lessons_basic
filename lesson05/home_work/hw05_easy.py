3c0a26e628a793ba9051ba68ecaf7a737d38a1d4
Murrow90 <40774595+Murrow90@users.noreply.github.com>
2018-07-16 22:56:07 +0300

    Update hw05_easy.py

--- lesson05/home_work/hw05_easy.py
+++ lesson05/home_work/hw05_easy.py
@@ -3,8 +3,39 @@
 # из которой запущен данный скрипт.
 # И второй скрипт, удаляющий эти папки.
 
+import os
+
+i = 1
+while i < 10:
+    dir_new = "dir_" + str(i)
+    dir_path = os.path.join(os.getcwd(), dir_new)
+    try:
+        os.makedir(dir_path)
+    except FileExistsError:
+    print("File exists")
+    i += 1
+
+remove = os.listdir(path = ".")
+for dir_new in remove:
+    os.rmdir(dir_new)
+#я его не проверяла, боюсь стереть себе весь диск =)
+
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
 
+import sys
+import os
+
+dir_list = os.listdir(path=".")
+print(dir_list)
+
+
 # Задача-3:
 # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
+
+import shutil
+
+new_file = open('hw_5.py', 'w')
+shutil.copy(r'hw_5.py', r'hw_5_copy.py')
+new_file.close()
+
